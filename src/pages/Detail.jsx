fb16c493d1d6e1e031535eef207f21ef50b72944
leegaeddong <118152590+leegaeddong@users.noreply.github.com>
2023-02-16 12:22:30 +0900

    메세지

---
+++ src/pages/Detail.jsx
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+
+const Detail = () => {
+  const todo = useSelector((state) => state.todos.todo);
+
+  const { id } = useParams();
+  console.log(id);
+  const navigate = useNavigate();
+  const todoslists = todo.find((todo) => {
+    console.log(Number(id));
+    return todo.id === Number(id);
+  });
+
+  return (
+    <StContainer>
+      <StDialog>
+        <div>
+          <StDialogHeader>
+            <div>ID :{todoslists.id}</div>
+            <StButton
+              borderColor="#ddd"
+              onClick={() => {
+                navigate("/");
+              }}
+            >
+              이전으로
+            </StButton>
+          </StDialogHeader>
+          <StTitle>{todoslists.title}</StTitle>
+          <StBody>{todoslists.body}</StBody>
+        </div>
+      </StDialog>
+    </StContainer>
+  );
+};
+
+export default Detail;
+
+const StContainer = styled.div`
+  border: 2px solid #eee;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StDialog = styled.div`
+  width: 600px;
+  height: 400px;
+  border: 1px solid #eee;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const StDialogHeader = styled.div`
+  display: flex;
+  height: 80px;
+  justify-content: space-between;
+  padding: 0 24px;
+  align-items: center;
+`;
+
+const StTitle = styled.h1`
+  padding: 0 24px;
+`;
+
+const StBody = styled.main`
+  padding: 0 24px;
+`;
+
+const StButton = styled.button`
+  border: 1px solid ${({ borderColor }) => borderColor};
+  height: 40px;
+  width: 120px;
+  background-color: #fff;
+  border-radius: 12px;
+  cursor: pointer;
+`;
